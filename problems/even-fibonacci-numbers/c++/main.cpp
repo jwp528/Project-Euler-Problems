a823353838b43180ffcfd7ad49bda835572006db
Josh <Parsons>
2020-04-30 00:26:37 -0400

    added solutions to the even-fibonacci-problem

---
+++ problems/even-fibonacci-numbers/c++/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    long sum = 0;
+    long value = 1;
+    long last = 1;
+    long max = 4000000;
+
+    for (;;)
+    {
+        if (value > max)
+            break;
+
+        if (value % 2 == 0)
+            sum += value;
+
+        long temp = value;
+        value += last;
+        last = temp;
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
